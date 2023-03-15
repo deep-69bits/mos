a14d1f2be09562930e5c9e62b27734ab7f3062db
deep-69bits <deepakchattwaniking15@gmail.com>
2023-03-16 02:44:47 +0530

    changes

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -25,12 +25,18 @@ const HomePage = () => {
             console.error("Error adding document: ", e);
           }
     }
+
+    const [date, setDate] = useState(new Date());
+    function refreshClock() {
+      setDate(new Date());
+    }
+
     useEffect(()=>{
-       
+        
       const func=async()=>{
         axios.get('http://api.aladhan.com/v1/calendarByCity/2023/4?city=Kuala%20Lumpu&country=malaysia&method=2')
         .then(function (response) {
-          console.log(response.data.data[0].timings);
+          console.log(response.data.data[0].timings.Asr);
         })
         .catch(function (error) {
           console.log(error);
@@ -39,9 +45,13 @@ const HomePage = () => {
         });
       }
       func()
+      const timerId = setInterval(refreshClock, 1000);
+      return function cleanup() {
+        clearInterval(timerId);
+      };
        
     },[])
-
+    
    
     const items = [
       <SidebarItem>Dashboard</SidebarItem>,
@@ -50,11 +60,17 @@ const HomePage = () => {
       <SidebarItem  > <Link to={'/hadid'}>Hadid</Link> </SidebarItem>,
       <SidebarItem  > <button onClick={()=>{signOut(auth)}}>logout</button> </SidebarItem>,
     ];
+
+    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
   return (
     <div className='h-screen bg-[#02062a]'>
     
     <Sidebar content={items} background="#000000" backdrop={true}>
     <div className='text-white'>
+    <h1 className='text-center mt-2'><span className='text-3xl'>{date.toLocaleTimeString()}</span><br /><span >{weekday[date.getDay()]} <span> </span>{date.getDate()}.{date.getMonth()}.{date.getUTCFullYear()}</span></h1>
+
+
 
     </div>
     </Sidebar>
